1cde2e5bc7ab308e232d5af7f48f3c8201db987d
WhatTheNasus <jefan.tv@gmail.com>
2025-01-23 05:32:36 +0100

    Refactor HomePage component to simplify session handling and improve readability

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,34 +1,20 @@
-// Import necessary modules
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/authOptions";
 // import AuthenticatedView from "@/sections/AuthHomeView";
 import GuestView from "@/sections/NonAuthHomeView";
 import { redirect } from "next/navigation";
 
-// Page metadata
 export const metadata = {
   title: "Domov | ZS"
 };
 
-/**
- * HomePage Component
- * This component dynamically renders content based on the user's authentication status.
- */
 export default async function HomePage() {
-  try {
-    // Attempt to retrieve the user session from the server
-    const userSession = await getServerSession(authOptions);
+  const userSession = await getServerSession(authOptions);
 
-    // Return the appropriate view based on session presence
-    if (userSession) {
-      redirect("/prispevok");
-      // return <AuthenticatedView session={userSession} />;
-    } else {
-      return <GuestView />;
-    }
-  } catch (error) {
-    console.error("Error fetching session:", error);
-    // Optionally, handle error state
-    return <GuestView />;
+  if (userSession) {
+    redirect("/prispevok");
+    return null;
   }
+
+  return <GuestView />;
 }
